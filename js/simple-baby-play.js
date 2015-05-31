fe13b57cc99df1a9547589dcc1eee60802464a15
Robert Pickering <robert@strangelights.com>
2015-05-31 13:26:11 +0200

    colouring and button pressing

---
+++ js/simple-baby-play.js
@@ -0,0 +1,22 @@
+function colorElement(elem, colorElem) {
+    // change element background color
+	if (elem.style.backgroundColor == "blue")
+	{
+		elem.style.backgroundColor = "red";
+	}
+	else
+	{
+		elem.style.backgroundColor = "blue";
+	}
+}
+window.onload = function() {
+    var elems = document.getElementsByTagName("div");
+    for(i = 0; i < elems.length; ++i)
+    {
+		if(elems[i].className == "col")
+		{
+			alert("setting");
+			elems[i].onclick = function() { colorElement(this); }
+		}
+    }
+}
